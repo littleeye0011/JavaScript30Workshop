8dc413926fa5db73c6d7bae675a82fe710eb529c
littleeye0011 <eiemily2014@gmail.com>
2022-05-03 00:11:42 +0700

    last commit

--- SearchSong/script.js
+++ SearchSong/script.js
@@ -24,21 +24,88 @@ async function searchLyrics(songs) {
   showData(allSong);
 }
 
-function showData(song) {
+function showData(songs) {
   result.innerHTML = `
      <ul class="songs">
-        ${song.data
+        ${songs.data
           .map(
-            (song) =>
+            (songs) =>
               `
                 <li><span>
-                <strong>${song.artist.name}</strong> - ${song.title}
+                <strong>${songs.artist.name}</strong> - ${songs.title}
                 </span>
-                <button class="btn">เนื้อเพลง</button>
+                <button class="btn"
+                  data-artist="${songs.artist.name}"
+                  data-song="${songs.title}"
+                >เนื้อเพลง</button>
                 </li>
             `
           )
           .join("")}
           </ul>
     `;
+  if (songs.next || songs.prev) {
+    more.innerHTML = `
+        ${
+          songs.prev
+            ? `<button class="btn" onclick="getPrevSongs('${songs.prev}')">ก่อนหน้า</button>`
+            : ""
+        }
+        ${
+          songs.next
+            ? `<button class="btn" onclick="getMoreSongs('${songs.next}')">ถัดไป</button>`
+            : ""
+        }
+      `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+async function getMoreSongs(songsUrl) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${songsUrl}`);
+
+  const allSong = await res.json();
+
+  showData(allSong);
+}
+async function getPrevSongs(songsUrl) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${songsUrl}`);
+
+  const allSong = await res.json();
+
+  showData(allSong);
+}
+
+result.addEventListener("click", (e) => {
+  const clickEl = e.target;
+
+  if (clickEl.tagName == "BUTTON") {
+    const artist = clickEl.getAttribute("data-artist");
+    const songName = clickEl.getAttribute("data-song");
+
+    getLyrics(artist, songName);
+  }
+});
+
+async function getLyrics(artist, songName) {
+  const res = await fetch(`${apiUrl}/v1/${artist}/${songName}`);
+
+  const data = await res.json();
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+
+  if (lyrics) {
+    result.innerHTML = `<h2><span>
+    <strong>${artist}</strong> - ${songName}
+    </span></h2>
+    <span>${lyrics}</span>
+    `;
+  } else {
+    result.innerHTML = `<h2><span>
+    <strong>${artist}</strong> - ${songName}
+    </span></h2>
+    <span>ไม่มีข้อมูล</span>
+    `;
+  }
+  more.innerHTML = "";
 }
